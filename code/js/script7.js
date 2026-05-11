7bae212aefea721ddb1fe91ee3ecf232016e7e97
Mohamed Ali <mode.wika0@gmail.com>
2026-05-11 21:17:47 +0300

    Update script7.js

--- code/js/script7.js
+++ code/js/script7.js
@@ -1,23 +1,18 @@
 function checkTwoKinds(s) {
-    // Check length exactly 4
     if (s.length !== 4) {
         return false;
     }
     
-    // Get unique characters
     const uniqueChars = new Set(s);
     
-    // Must have exactly 2 different characters
     if (uniqueChars.size !== 2) {
         return false;
     }
     
-    // Convert set to array and count occurrences
     const chars = Array.from(uniqueChars);
     const count1 = s.split(chars[0]).length - 1;
     const count2 = s.split(chars[1]).length - 1;
     
-    // Each must appear exactly 2 times
     return count1 === 2 && count2 === 2;
 }
 
